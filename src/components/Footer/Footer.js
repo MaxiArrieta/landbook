7d9f813dc152b7fac46d3aff3fc046dc6bd53b7e
Maxi Arrieta <arrieta.maxi18@gmail.com>
2021-02-16 11:48:30 -0300

    agregando footer y correccion de estilos

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,75 @@
 import React from "react"
+import styled from "styled-components"
 
 export default function Footer() {
   return (
-    <div>
-      <h3>Hola</h3>
-    </div>
+    <Section>
+      <Div>
+        <div>
+          <ul>
+            <Li>LandBook</Li>
+            <li>Copyright 2021</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <Li>Síguenos</Li>
+            <li>Instagram</li>
+            <li>Facebook</li>
+          </ul>
+        </div>
+        <div>
+          <ul>
+            <Li>Contacto</Li>
+            <li>hello@landbook.app</li>
+            <li>Affiliate Program</li>
+          </ul>
+        </div>
+      </Div>
+      <DivBy>
+        <p>
+          Web realizada por
+          <a href="mailto:arrieta.maxi18@gmail.com"> Maximiliano Arrieta</a>
+        </p>
+      </DivBy>
+    </Section>
   )
 }
+
+export const Section = styled.footer`
+  width: 100%;
+`
+export const Div = styled.div`
+  display: flex;
+  justify-content: space-around;
+
+  div {
+    padding: 40px;
+    margin-bottom: 15px;
+  }
+  ul {
+    list-style: none;
+  }
+
+  li {
+    font-size: 0.8em;
+  }
+`
+
+export const Li = styled.li`
+  font-weight: bold;
+  margin-bottom: 15px;
+`
+
+export const DivBy = styled.div`
+  background-color: #d7d7d8;
+  p {
+    font-size: 0.8em;
+    text-align: center;
+  }
+  a {
+    text-decoration: none;
+    color: black;
+    font-weight: bold;
+  }
+`
--- src/components/Start/styles.js
+++ src/components/Start/styles.js
@@ -2,7 +2,7 @@ import styled from "styled-components"
 
 export const Section = styled.section`
   width: 100%;
-  height: 960px;
+  height: 860px;
   background-color: var(--secondary);
   color: var(--light);
   padding-top: 20px;
